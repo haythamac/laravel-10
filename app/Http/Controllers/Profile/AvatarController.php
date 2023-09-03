4a80ac8b95107ad661f40f62f1f4fe346cb0d907
haythamac <janssen.uy.dev@gmail.com>
2023-09-03 18:53:43 +0800

    Storage Facade: delete

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
@@ -33,6 +34,10 @@ class AvatarController extends Controller
     {
 
         $path = $request->file('avatar')->store('avatars', 'public');
+        if($oldAvatar = $request->user()->avatar)
+        {
+            Storage::disk('public')->delete($oldAvatar);
+        }
         auth()->user()->update(['avatar' => $path]);
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
     }
